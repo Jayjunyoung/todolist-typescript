88abbdf5c708890065fa476bdac0f9f83678a361
junyoung <no2jfamily@tukorea.ac.kr>
2022-08-31 13:32:03 +0900

    0831

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,122 +1,31 @@
-import React, { useState } from 'react';
-import { useForm } from "react-hook-form";
-import { errorSelector } from 'recoil';
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 
+import { Categories, categoryState, toDoSelector } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
-/*function ToDoList() {//event에 대해서 폼이라는걸 알려주는것
-    const [todo, setTodo] = useState("");
-    const [toDoError, setToDoError] = useState("");
-    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-        const {currentTarget : { value },
-        } = event; //ES6 문법
-        setToDoError("");
-        setTodo(value);//currentTarget value로 업데이트를 함
-    }
-    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if(todo.length < 10) {
-            return setToDoError("To do가 더 길어야됌");
-        }
-    }
-    return <div>
-        <form onSubmit={onSubmit}>
-            <input onChange = {onChange} value = {todo} placeholder="Write a to do" />
-            <button>Add</button>
-            {toDoError !== "" ? toDoError : null}
-        </form>
-    </div>
+function ToDoList() {
+    const toDos = useRecoilValue(toDoSelector);
+    const [category, setCategory] = useRecoilState(categoryState);
+    const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+        setCategory(event.currentTarget.value as any);
+    };
+    console.log(toDos);
+    return (
+        <div>
+            <h1>To Dos</h1>
+            <hr />
+            <select value={category} onInput={onInput}>
+                <option value={Categories.TO_DO}>To Do</option>
+                <option value={Categories.DOING}>Doing</option>
+                <option value={Categories.DONE}>Done</option>
+            </select>
+            <CreateToDo />
+            {toDos?.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+            ))}
+        </div>
+    );
 }
-    
-*/
-interface IForm {//타입스크립트 에게 폼의 형식 알려주기
-    Email: string;
-    FirstName: string;
-    LastName: string;
-    Username: string;
-    Password: string;
-    Password1: string;
-    extraError?: string;//필수가 아님(?)
-}
-
-
-function ToDoList() {//0829
-    const { //모든 기능들이 useForm안에 담겨있다
-        register , 
-        handleSubmit, 
-        setValue,
-        formState:{ errors },//에러가 있으면 보여주는 객체
-        setError, //에러를 발생시키는 함수
-    } = useForm<IForm>({
-        defaultValues: {//폼의 기본값 설정
-            Email: "@naver.com",
-        }
-    });//react-hook-form
-    //watch: input에 입력된 값 모두 추적
-    const onValid = (data: IForm) => {//데이터가 유효할때 발동된다
-        if(data.Password !== data.Password1) {//data 인자로 사용
-            setError(
-            "Password1", 
-            {message : "비밀번호 달라요"}, 
-            {shouldFocus:true});
-        }
-        setValue("Email", "");//Email input값 빈칸으로 놔두기
-        //setError("extraError", {message: "서버 해킹"});//전체폼에 대한 에러
-        //extraError라는 걸 새로 만들어서 추가적인 에러를 발생시킬수 있다
-    }
-    console.log(errors);//에러가 어디서 났는지 알려줌
-
-    //onChange랑 value useState 모두 대체
-    //레지스터함수를통해 name에 toDo 라는 객체들감
-
-    //<onValid>
-    //데이터가 유효할때 발생하는 onValid 함수 호출(필수)
-    //모든 검증에서 에러가 없었다는걸 의미한다
-
-    //handleSubmit이 발동되어야 모든 validation 수행 
-    return <div>
-        <form style = {{display:"flex", flexDirection:"column"}} onSubmit={handleSubmit(onValid)}>
-            <input {...register("Email", {
-                required:"Email is required", //아예 작성이 안돈경우엔 이게뜸
-                pattern: {//정규식으로 조건 주기!: naver.com으로 끝나는 이메일만 허용
-                    value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                    message: "Only naver.com emails allowed",//이메일 형식이 잘못되었을 경우 뜸
-            },
-            })} 
-                placeholder="Email" 
-            />
-            <span>{errors?.Email?.message} </span>
-            <input {...register("FirstName", 
-            {required:"write here", 
-            validate: (value) => value.includes("nico") ? "do not include nico" : true}
-            )} 
-            placeholder="FirstName" 
-            />
-            <span>{errors?.FirstName?.message} </span>
-            <input {...register("LastName", {required:"write here"})} 
-            placeholder="LastName" 
-            />
-            <span>{errors?.LastName?.message} </span>
-            <input {...register("Username", {required:"write here"})} 
-            placeholder="Username" 
-            />
-            <span>{errors?.Username?.message} </span>
-            <input {...register("Password", {
-                required:"Password is required", 
-                minLength: {
-                    value: 10,
-                    message:"Your Password is short",
-                },
-            })} 
-            placeholder="Password" 
-            />
-            <span>{errors?.Password?.message} </span>
-            <input {...register("Password1", {required:"write here"})} placeholder="Passcheck" />
-            <span>{errors?.Password1?.message} </span>
-            <button>Add</button>
-            <span>{errors?.extraError?.message}</span>
-        </form>
-    </div>
-}
-
-
 export default ToDoList;
